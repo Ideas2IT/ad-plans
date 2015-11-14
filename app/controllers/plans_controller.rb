528985ead78a84536ee02e11468b99f42615683c
rabinprithvi <rabindranath.prithvi@gmail.com>
2015-11-14 20:28:49 +0530

    Create Plan API -  Able to create plan and its features in one post request

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -1,4 +1,5 @@
 class PlansController < ApplicationController
+  skip_before_filter :verify_authenticity_token, :only => [:create]
   respond_to :json
 
   def index
@@ -6,5 +7,14 @@ class PlansController < ApplicationController
       format.json {render :json => Plan.all, :include => {:features => {:only => [:id, :name, :limit]}}, :except => [:created_at, :updated_at]}
       format.html
     end
+  end
+
+  def create
+    respond_with Plan.create(plan_params)
+  end
+
+  private
+  def plan_params
+    params.require(:plan).permit(:name, :description, :price, features_attributes: [:id, :name, :limit])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :plans, only: [:index]
+  resources :plans
 
   root to: 'visitors#index'
   devise_for :users
